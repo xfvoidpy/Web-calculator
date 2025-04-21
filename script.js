b56c5223424d1a785ac0d1041551c25e4cc21de4
Mert Dursun <mrttdrsn1@gmail.com>
2025-04-21 17:34:53 +0300

    Sesler eklendi , mobilde tuşların mavileşmesi kapatıldı

--- script.js
+++ script.js
@@ -30,3 +30,43 @@ function toggleSign() {
     display.value = currentInput;
   }
 }
+
+const clickSound = new Audio('sound/sound.mp3'); // Ses dosyasını buraya ekle
+
+function playClickSound() {
+  clickSound.play(); // Butona tıklandığında ses çalar
+}
+
+function appendNumber(value) {
+  playClickSound(); // Tuşa basıldığında ses çal
+  currentInput += value;
+  display.value = currentInput;
+}
+
+function clearDisplay() {
+  playClickSound(); // Temizleme tuşuna basıldığında ses çal
+  currentInput = '';
+  display.value = '';
+}
+
+function calculate() {
+  playClickSound(); // Hesaplama tuşuna basıldığında ses çal
+  try {
+    currentInput = eval(currentInput.replace('÷', '/').replace('×', '*')).toString();
+    display.value = currentInput;
+  } catch {
+    display.value = 'Error';
+  }
+}
+
+function toggleSign() {
+  playClickSound(); // ± tuşuna basıldığında ses çal
+  if (currentInput) {
+    if (currentInput.startsWith('-')) {
+      currentInput = currentInput.slice(1);
+    } else {
+      currentInput = '-' + currentInput;
+    }
+    display.value = currentInput;
+  }
+}
